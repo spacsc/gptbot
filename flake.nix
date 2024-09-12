070d7529d278d20014d51e43fbf6ef62c027f985
foehammer127 <foehammer@disroot.org>
2024-09-12 13:38:28 -0500

    Add Development Environment

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} (toplevel @ {withSystem, ...}: {
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-linux"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          localSystem = system;
+          config = {
+            allowUnfree = true;
+            allowAliases = true;
+          };
+          overlays = [];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [];
+        };
+
+        formatter = inputs'.alejandra.packages.default;
+      };
+    });
+}
